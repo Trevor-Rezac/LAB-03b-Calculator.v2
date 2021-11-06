4a1269c7ea05f492f6588c695c4c893834c0d9d7
TrevorRezac <trev.rezac@gmail.com>
2021-11-05 17:17:33 -0700

    built functioning calculator

--- app.js
+++ app.js
@@ -1,8 +1,86 @@
 // import functions and grab DOM elements
+import { add, subtract, multiply, divide, convert } from '../math-utils.js'; 
 
-// initialize state
+// addition section 
+const addNum1 = document.getElementById('add-num-1');
+const addNum2 = document.getElementById('add-num-2');
+const addButton = document.getElementById('add-button');
+const sumTotal = document.getElementById('sum');
 
 // set event listeners 
+addButton.addEventListener('click', () => {
+  
   // get user input
+    const x = +addNum1.value;
+    const y = +addNum2.value;
+
   // use user input to update state 
+
+    const sum = add(x, y);
   // update DOM to reflect the new state
+
+    sumTotal.textContent = sum;
+});
+
+// subtraction section 
+
+const subNum1 = document.getElementById('sub-num-1');
+const subNum2 = document.getElementById('sub-num-2');
+const subtractButton = document.getElementById('subtract-button');
+const subOutcome = document.getElementById('difference');
+
+subtractButton.addEventListener('click', () => {
+
+    const x = +subNum1.value;
+    const y = +subNum2.value;
+    const difference = subtract(x, y);
+
+    subOutcome.textContent = difference;
+});
+
+// multiplication section 
+
+const multNum1 = document.getElementById('multiply-num-1');
+const multNum2 = document.getElementById('multiply-num-2');
+const multButton = document.getElementById('multiply-button');
+const multOutcome = document.getElementById('product');
+
+multButton.addEventListener('click', () => {
+
+    const x = +multNum1.value;
+    const y = +multNum2.value;
+
+    const product = multiply(x, y);
+
+    multOutcome.textContent = product;
+});
+
+// division section 
+
+const divNum1 = document.getElementById('divide-num-1');
+const divNum2 = document.getElementById('divide-num-2');
+const divButton = document.getElementById('divide-button');
+const divOutcome = document.getElementById('divisor');
+
+divButton.addEventListener('click', () => {
+
+    const x = +divNum1.value;
+    const y = +divNum2.value;
+
+    const divisor = divide(x, y);
+
+    divOutcome.textContent = divisor;
+});
+
+// celsius to fahrenheit section 
+
+const celNum = document.getElementById('celsius');
+const celButton = document.getElementById('convert-button');
+const celOutcome = document.getElementById('fahrenheit');
+
+celButton.addEventListener('click', () => {
+    const x = celNum.value;
+    const fahrenheit = convert(x);
+
+    celOutcome.textContent = fahrenheit;
+});
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,86 @@
-import './example.test.js';
+// import './example.test.js';
+import { add, subtract, multiply, divide, convert } from '../math-utils.js';
+
+const test = QUnit.test;
+
+// addition test
+test('add() should take in two numbers 1 and 1 and return 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 1;
+    const y = 1;
+    const expected = 2;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+console.log(add(1, 1));
+
+// subtraction test 
+test('subtract() should take in two numbers 2 and 1 and return 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 2;
+    const y = 1;
+    const expected = 1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(x, y);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+console.log(subtract(2, 1));
+
+// multiplication test
+
+test('multiply() should take in two numbers 2 and 2 and return 4', (expect) => {
+
+    const x = 2;
+    const y = 2;
+    const expected = 4;
+
+    const actual = multiply(x, y);
+
+    expect.equal(actual, expected);
+});
+
+console.log(multiply(2, 2));
+
+// division test
+
+test('divide() should take in two numbers 2 and 2 and return 1', (expect) => {
+
+    const x = 2;
+    const y = 2;
+    const expected = 1;
+
+    const actual = divide(x, y);
+
+    expect.equal(actual, expected);
+});
+
+console.log(divide(2, 2));
+
+// celsius conversion test 
+
+test('convert() should takes one number 32 and returns 89.6', (expect) => {
+
+    const x = 32;
+    const expected = 89.6;
+
+    const acctual = convert(x);
+
+    expect.equal(acctual, expected);
+});
+
+console.log(convert(32));
